320f3c82689282efde0c3b665a4e7d54490e9552
Lautaro <lautarodm98@gmail.com>
2024-04-08 15:00:35 -0300

    routes added

--- src/app.js
+++ src/app.js
@@ -1,19 +1,22 @@
 import express from 'express';
 import morgan from 'morgan';
 
+import tasksRoutes from './routes/tasks.routes.js';
+import authRoutes from './routes/auth.routes.js';
+
 const app = express();
 
+// middlewares
 app.use(morgan('dev'));
-
-// middlewares para 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// rutas
 app.get('/', (req, res) => {
   res.json({ message: "welcome to my API" })
 })
-
-
+app.use("/api", tasksRoutes)
+app.use("/api", authRoutes)
 
 
 
@@ -22,6 +25,7 @@ app.get('/test', (req, res) => {
   res.send("test")
 })
 
+// error handler
 // middleware que va abajo de todo y asi manejamos los errores
 app.use((err, req, res, next) => {
   res.status(500).json({
